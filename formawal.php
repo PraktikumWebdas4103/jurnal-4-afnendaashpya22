c2fb16f3f3f37748b30d37092d44c12ef006de3e
afnendaashpya22 <43516021+afnendaashpya22@users.noreply.github.com>
2018-09-25 16:55:14 +0700

    Add files via upload

--- formawal.php
+++ formawal.php
@@ -1,5 +1,30 @@
 <?php
+	session_start();
+	error_reporting(0);
+		$user = array('user' => 'afnenda',
+					  'pass' => 'afnenda');
+			if (isset($_POST['submit'])) {
+				if ($_POST['username'] == $user['user'] && $_POST['password'] == $user['pass']) {
+					$_SESSION['username'] = $_POST['username'];
+					echo "Anda Berhasil Login $_POST[username]!!!"; echo "<br>";
+					echo "<a href = 'proses.php'>Klik disini</a>";
+				}
+				else{
+					display_login_form();
+					echo "Error!!!";
+				}
+			}
+			else{
+				display_login_form();
+			}
 
-
-
+	function display_login_form(){ ?>
+		<form action = '<?php echo $_SERVER['PHP_SELF']; ?>' method = 'POST';">
+			<label for='username'>Username</label>
+			<input type="text" name="uname" id="username">
+			<label for="password">Password</label>
+			<input type="password" name="password" id="password">
+			<input type="submit" name="submit" value="Login">
+		</form>
+	<?php }
 ?>
--- proses.php
+++ proses.php
@@ -1,4 +1,31 @@
+<form action="" method="POST">
+	<table>
+		<tr>
+			<td>Nama</td>
+			<td>:</td>
+			<td><input type="text" name="nama"></td>
+		</tr>
+		<tr>
+			<td>Hobi</td>
+			<td>:</td>
+			<td><input type="checkbox" name="hobi[]" value="baca">Membaca Novel</td>
+			<td><input type="checkbox" name="hobi[]" value="musik">Bermusik</td>
+			<td><input type="checkbox" name="hobi[]" value="travelling">Travelling</td>
+		</tr>
+		<tr>
+			<td>Gambar</td>
+			<td>:</td>
+			<td><input type="file" name="file"></td>
+		</tr>
+		<tr>
+			<td><input type="submit" name="submit" value="Submit"></td>
+		</tr>
+	</table>
+</form>
+
 <?php
-
-
+	if (isset($_POST['submit'])) {
+		$nama = $_POST['nama'];
+		$hobi = $_POST['hobi'];
+	}
 ?>
